cae134f989e56c48355ef9719da79a4bd338a5bb
rosenmai <20245897+maikroservice@users.noreply.github.com>
2022-03-03 22:39:24 +0100

    blocking requests work for now

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,11 @@
 use clap::Parser;
 use std::boxed::Box;
-// use url::{ParseError, Url};
+
+use reqwest;
 
 use std::fs::File;
 use std::io::prelude::*; // we need that for BufReader lines
 use std::io::{BufReader, Error};
-use std::net::Ipv4Addr;
-
-fn main() {
-    let args = Cli::parse();
-    let path = &args.wordlist;
-
-    let wordlist = open_wordlist(path).unwrap();
-    for line in wordlist {
-        println!("{}", line);
-    }
-}
-#[derive(Parser)]
-pub struct Cli {
-    wordlist: String,
-    #[clap(parse(from_os_str))]
-    ip: Ipv4Addr,
-}
 
 /*
 TODO:
@@ -37,10 +21,31 @@ TODO:
 - define default wordlist
 */
 
+fn main() {
+    let args = Cli::parse();
+    let path = &args.wordlist;
+    let ip = &args.ip;
+    let _connection = http_request(ip);
+    let _wordlist = open_wordlist(path).unwrap();
+}
+#[derive(Parser)]
+pub struct Cli {
+    wordlist: String,
+    ip: String,
+}
+
 fn open_wordlist(path: &str) -> Result<Box<impl Iterator<Item = String>>, Error> {
     let file = File::open(path)?;
     let reader = BufReader::new(file)
         .lines()
         .map(|line| line.unwrap_or_default());
     Ok(Box::new(reader))
+}
+
+fn http_request(ip: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let uri = format!("http://{}", ip);
+    println!("{}", uri);
+    let resp = reqwest::blocking::get(uri)?;
+    println!("{:#?}", resp.status());
+    Ok(())
 }
