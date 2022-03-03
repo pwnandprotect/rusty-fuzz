55b09c194a19d36e91276b0e01de81fb6026297a
Maik Ro <20245897+maikroservice@users.noreply.github.com>
2022-03-03 21:26:53 +0100

    update comments

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::boxed::Box;
 use std::fs::File;
 use std::io::prelude::*; // we need that for BufReader lines
 use std::io::{BufReader, Error};
+use std::net::Ipv4Addr;
 
 fn main() {
     let args = Cli::parse();
@@ -15,6 +16,26 @@ fn main() {
         println!("{}", line);
     }
 }
+#[derive(Parser)]
+pub struct Cli {
+    wordlist: String,
+    #[clap(parse(from_os_str))]
+    ip: Ipv4Addr,
+}
+
+/*
+TODO:
+- add IP to cli arguments
+- make sync request to IP / url
+- take response status code
+- check if response status code is within the list of allowed status codes?
+- create cli table
+- make async via tokio
+- add thread limits
+- add extension cli parameter
+- use extension parameter to fuzz for files
+- define default wordlist
+*/
 
 fn open_wordlist(path: &str) -> Result<Box<impl Iterator<Item = String>>, Error> {
     let file = File::open(path)?;
@@ -22,9 +43,4 @@ fn open_wordlist(path: &str) -> Result<Box<impl Iterator<Item = String>>, Error>
         .lines()
         .map(|line| line.unwrap_or_default());
     Ok(Box::new(reader))
-}
-
-#[derive(Parser)]
-pub struct Cli {
-    wordlist: String,
 }
